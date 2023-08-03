411fb672dc219b2c698e5287c82b5d0609dfe24b
qkrvudtn98 <ps3624@naver.com>
2023-08-04 04:38:20 +0900

    스플래시 화면 구현

--- app/src/main/java/com/flatwater/ttwwi/SplashActivity.kt
+++ app/src/main/java/com/flatwater/ttwwi/SplashActivity.kt
@@ -1,11 +1,18 @@
 package com.flatwater.ttwwi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.flatwater.ttwwi.auth.SignUpActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }, 1500)
     }
 }
