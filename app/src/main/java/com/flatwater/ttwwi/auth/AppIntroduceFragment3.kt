a80a4ea062248842cb4fb7d3af877596661c0349
qkrvudtn98 <ps3624@naver.com>
2023-08-04 15:19:00 +0900

    App Introduction View Pager 적용

---
+++ app/src/main/java/com/flatwater/ttwwi/auth/AppIntroduceFragment3.kt
@@ -0,0 +1,19 @@
+package com.flatwater.ttwwi.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.flatwater.ttwwi.R
+
+class AppIntroduceFragment3 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_app_introduce3, container, false)
+    }
+
+}
