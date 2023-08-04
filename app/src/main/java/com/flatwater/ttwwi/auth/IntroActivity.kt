afbd7d18ea4c55e2c114eaf5328147b0aa729b22
qkrvudtn98 <ps3624@naver.com>
2023-08-04 15:18:23 +0900

    Fragment Adapter 전환 (View Adapter 삭제)

--- app/src/main/java/com/flatwater/ttwwi/auth/IntroActivity.kt
+++ app/src/main/java/com/flatwater/ttwwi/auth/IntroActivity.kt
@@ -1,22 +1,31 @@
 package com.flatwater.ttwwi.auth
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.flatwater.ttwwi.R
 import com.flatwater.ttwwi.databinding.ActivityIntroBinding
+
+private const val NUM_PAGES = 5
 
 class IntroActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityIntroBinding
 
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
 
-        binding.viewPagerAppIntroduction.adapter = ViewPagerAdapter(getAppIntroduction())
-        binding.viewPagerAppIntroduction.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        viewPager = binding.viewPagerAppIntroduction
+        val pagerAdapter = AppIntroducePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
 
         binding.socialLoginKakaoBtn.setOnClickListener {
             // kakao 소셜 로그인 연결 예정
@@ -27,11 +36,15 @@ class IntroActivity : AppCompatActivity() {
         }
     }
 
-    private fun getAppIntroduction(): ArrayList<Int> {
-        return arrayListOf<Int> (
-            R.drawable.illustration1,
-            R.drawable.illustration2,
-            R.drawable.illustration3
-        )
+    private inner class AppIntroducePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun createFragment(position: Int): Fragment {
+            return when(position){
+                0 -> AppIntroduceFragment1()
+                1 -> AppIntroduceFragment2()
+                else -> AppIntroduceFragment3()
+            }
+        }
     }
 }
