b28656b6f96644446288f6a9c8561f1e537fece1
qkrvudtn98 <ps3624@naver.com>
2023-08-11 23:52:51 +0900

    kakao social login - 마이페이지 버튼 테스트

--- app/src/main/java/com/flatwater/ttwwi/MainActivity.kt
+++ app/src/main/java/com/flatwater/ttwwi/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.flatwater.ttwwi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.flatwater.ttwwi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.goMyPage.setOnClickListener {
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
