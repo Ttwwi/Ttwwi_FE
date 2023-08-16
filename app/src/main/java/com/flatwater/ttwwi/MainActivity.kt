d3f921af46802219c50e4310173f802fd9826485
qkrvudtn98 <ps3624@naver.com>
2023-08-16 20:33:21 +0900

    #P : Bottom Navigation Bar 기능 구현 완료

--- app/src/main/java/com/flatwater/ttwwi/MainActivity.kt
+++ app/src/main/java/com/flatwater/ttwwi/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.flatwater.ttwwi
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.flatwater.ttwwi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +12,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.goMyPage.setOnClickListener {
-            val intent = Intent(this, MyPageActivity::class.java)
-            startActivity(intent)
-        }
+        initBinding()
+        initNavigation()
+    }
+
+    private fun initBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initNavigation() {
+        NavigationUI.setupWithNavController(binding.navBar, findNavController(R.id.nav_host))
     }
 }
