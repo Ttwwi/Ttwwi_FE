49320d2e2b77c44396272a9d6d8dd35b784c2c0c
qkrvudtn98 <ps3624@naver.com>
2023-08-18 14:22:49 +0900

    #P : Kakao Social Login Retrofit Interface

---
+++ app/src/main/java/com/flatwater/ttwwi/auth/LoginService.kt
@@ -0,0 +1,29 @@
+package com.flatwater.ttwwi.auth
+
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface LoginService {
+    @POST("backend https:// 제거한 uri")
+
+    fun postAccessToken(
+        //@Header("access_token") token : String
+        @Body jsonParams : UserModel
+    ) : Call<LoginBackendResponse>
+
+    companion object {
+        private val gson = GsonBuilder().setLenient().create()
+        fun loginRetrofit() : LoginService {
+            return Retrofit.Builder()
+            .baseUrl("") // backend server address
+            // .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(LoginService::class.java)
+        }
+    }
+}
