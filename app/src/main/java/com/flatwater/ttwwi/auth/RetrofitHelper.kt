b3a62e8e02509988ab9bf42d951aa61af9e6b8b9
qkrvudtn98 <ps3624@naver.com>
2023-08-18 14:22:30 +0900

    #P : Kakao Social Login Retrofit Object (Client)

---
+++ app/src/main/java/com/flatwater/ttwwi/auth/RetrofitHelper.kt
@@ -0,0 +1,21 @@
+package com.flatwater.ttwwi.auth
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitHelper {
+
+    val BASE_URL: String = "" // backend server address
+    var gson = GsonBuilder().setLenient().create()
+
+    fun getRetrofitInstance(): Retrofit {
+        val builder: Retrofit.Builder = Retrofit.Builder()
+        val retrofit = builder.baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        return retrofit
+    }
+
+}
