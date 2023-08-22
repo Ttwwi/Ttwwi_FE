537f607ec295e12bfb1609c36ff6b259a04a5576
qkrvudtn98 <ps3624@naver.com>
2023-08-22 13:17:51 +0900

    #P : 여행 피드 페이지 연결 버튼 생성 (임시)

--- app/src/main/java/com/flatwater/ttwwi/fragment/EditTripFragment.kt
+++ app/src/main/java/com/flatwater/ttwwi/fragment/EditTripFragment.kt
@@ -1,11 +1,13 @@
-package com.flatwater.ttwwi
+package com.flatwater.ttwwi.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.flatwater.ttwwi.databinding.FragmentEditTripBinding
+import com.flatwater.ttwwi.fragment.feed.TripFeedActivity
 
 // EditTripPage 작업 예정 (8/18 ~ )
 class EditTripFragment : Fragment() {
@@ -17,6 +19,14 @@ class EditTripFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentEditTripBinding.inflate(inflater, container, false)
+
+        // 여행 피드 페이지로 연결되도록 설정 (임시)
+        binding.makeTripBtn.setOnClickListener {
+            val intent = Intent(activity, TripFeedActivity::class.java)
+            startActivity(intent)
+        }
+
         return binding.root
+
     }
 }
