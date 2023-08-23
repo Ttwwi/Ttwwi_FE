70200f0148a6617d082835c75ccf6896611b87b4
qkrvudtn98 <ps3624@naver.com>
2023-08-23 14:36:22 +0900

    #P : EditTripPage Mark Up -> Event 등록 작업 (일부)

--- app/src/main/java/com/flatwater/ttwwi/fragment/EditTripFragment.kt
+++ app/src/main/java/com/flatwater/ttwwi/fragment/EditTripFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.flatwater.ttwwi.databinding.FragmentEditTripBinding
 import com.flatwater.ttwwi.fragment.feed.TripFeedActivity
 
@@ -18,7 +19,28 @@ class EditTripFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         binding = FragmentEditTripBinding.inflate(inflater, container, false)
+
+        // 제목 입력 -> 여행 표지 페이지에 반영
+
+        // 비밀번호 입력 -> 여행 표지 페이지에 입력해야 들어갈 수 있도록 설정
+
+        // 이미지 아이콘 터치 -> 갤러리 & 카메라 연결 -> 사진 등록 버튼 터치 -> 프로필 사진 등록 완료
+        binding.tripProfilePhotoRegisterButton.setOnClickListener {
+            // 갤러리로 연결 (사진 선택해서 binding.tripProfilePhoto로 데이터 주입)
+        }
+
+        // 소셜 친구 초대 (소셜 로그인 기능 연결된 후 연결 예정)
+        binding.kakaoInviteFriend.setOnClickListener {
+            Toast.makeText(activity, "kakao 친구 초대 기능 구현 예정입니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.instagramInviteFriend.setOnClickListener {
+            Toast.makeText(activity, "instagram 팔로워 초대 기능 구현 예정입니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        // 부제 & 설명 -> 여행 표지 페이지에 반영 (선택 사항)
 
         // 여행 피드 페이지로 연결되도록 설정 (임시)
         binding.makeTripBtn.setOnClickListener {
