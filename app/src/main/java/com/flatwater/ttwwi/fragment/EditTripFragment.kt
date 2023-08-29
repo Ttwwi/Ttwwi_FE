b6cc926063f06e9515345682a2cdb7ea8d070fda
qkrvudtn98 <ps3624@naver.com>
2023-08-29 19:32:14 +0900

    #P : Edit Trip Page specific feature plan

--- app/src/main/java/com/flatwater/ttwwi/fragment/EditTripFragment.kt
+++ app/src/main/java/com/flatwater/ttwwi/fragment/EditTripFragment.kt
@@ -1,6 +1,7 @@
 package com.flatwater.ttwwi.fragment
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.flatwater.ttwwi.databinding.FragmentEditTripBinding
 import com.flatwater.ttwwi.fragment.feed.TripFeedActivity
+import com.flatwater.ttwwi.viewobjects.EditTripViewObject
 
 // EditTripPage 작업 예정 (8/18 ~ )
 class EditTripFragment : Fragment() {
@@ -22,7 +24,16 @@ class EditTripFragment : Fragment() {
 
         binding = FragmentEditTripBinding.inflate(inflater, container, false)
 
+
+//        0. 여행 제작 페이지
+//        - 프로필 사진 등록 : 버튼 클릭 - 갤러리 접근 - 선택한 이미지 프로필 사진란에 랜더링 - 사진 등록하기 버튼 클릭 시 프로필 이미지 uri 데이터 모델에 저장
+//        - 비밀번호 : 정규표현식 사용하여 로직 부여 (예 : 8 ~ 16자 / 영문 + 숫자 + 특수문자 혼용)
+//        - 소셜로그인 친구초대 기능 구현 : 소셜 로그인 친구 초대 버튼 클릭 시 연결되어 있는 소셜 로그인의 친구 초대할 수 있는 기능 구현 (해당 소셜 로그인 기능 구현 시 적용)
+//        - 제목 / 부제 / 설명 / 비밀번호 / 친구 목록 (새로 추가된 내용) 데이터 모델 적용
+
         // 제목 입력 -> 여행 표지 페이지에 반영
+        val title = binding.tripName.toString()
+        println(title)
 
         // 비밀번호 입력 -> 여행 표지 페이지에 입력해야 들어갈 수 있도록 설정
 
